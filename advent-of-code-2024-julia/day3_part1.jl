e1597e000692f54b45968db4e823149c5da19e00
Matt OP <matthewhill.op@gmail.com>
2024-12-16 11:01:49 -0500

    Update day3_part1.jl

--- advent-of-code-2024-julia/day3_part1.jl
+++ advent-of-code-2024-julia/day3_part1.jl
@@ -1,23 +1,27 @@
-using DelimitedFiles
-using DataFrames
-
 lines = []
 
-open("C:2024inputday3.txt", "r") do file
+open("2024inputday3.txt", "r") do file
     for line in eachline(file)
         push!(lines, line)
     end
 end
 
 pattern = r"mul\(\d{1,3},\d{1,3}\)"
+parenthesis = r"\(([^)]+)\)"
 
-function find_sum()
+function find_sum()::Int
     total = 0
     for line in lines
         for m in eachmatch(pattern, line)
-            println(m.match)
-            total += 1
+            match_result = match(parenthesis, m.match)
+            captures = split(match_result.captures[1], ",")
+            num1 = parse(Int, captures[1])
+            num2 = parse(Int, captures[2])
+            total += num1 * num2
         end
     end
     return total
 end
+
+total = find_sum()
+println(total)
